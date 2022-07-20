6b4407e3019fd85b624ad2ee626f3079ec5dc412
Abbas Hejazi <AbbasHejazi@dataflow.local>
2022-07-20 03:38:43 +0300

    deploy

--- api/controllers/goalController.js
+++ api/controllers/goalController.js
@@ -27,14 +27,13 @@ const updateGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Goal Not found")
     }
-    const user = await User.findById(req.user.id)
 
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
